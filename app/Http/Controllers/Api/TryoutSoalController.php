032e3dd0500ea9101ed571e1174d42961e251f62
Bernaldo Napitupulu <bernaldonapitupulu@Bernaldos-MacBook-Pro.local>
2026-02-02 22:17:38 +0800

    Kembalikan login

--- app/Http/Controllers/Api/TryoutSoalController.php
+++ app/Http/Controllers/Api/TryoutSoalController.php
@@ -26,7 +26,6 @@ class TryoutSoalController extends Controller
                     'id'         => $item->banksoal->id,
                     'pertanyaan' => $item->banksoal->pertanyaan,
                     'urutan'     => $item->urutan,
-                    'poin'       => $item->poin,
                 ];
             });
 
@@ -60,8 +59,6 @@ class TryoutSoalController extends Controller
                     'jawaban_label' => $jawabanLabel,
                     'pembahasan'    => $item->banksoal->pembahasan,
                     'urutan'        => $item->urutan,
-                    // ⬇️ POIN DARI tryout_soal
-                    'poin'          => $item->poin,
                     'opsi' => $opsi->map(function ($o) {
                         return [
                             'id'         => $o->id,
@@ -159,20 +156,5 @@ class TryoutSoalController extends Controller
         ]);
     }
 
-    public function updatePoin(Request $request, $tryoutId, $banksoalId)
-    {
-        $data = $request->validate([
-            'poin' => 'required|integer|min:0'
-        ]);
-
-        TryoutSoal::where('tryout_id', $tryoutId)
-            ->where('banksoal_id', $banksoalId)
-            ->update([
-                'poin' => $data['poin']
-            ]);
-
-        return response()->json([
-            'message' => 'Poin berhasil diperbarui'
-        ]);
-    }
+    
 }
